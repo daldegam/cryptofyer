4a2925d6a3c888b54538fc6ba80fcbef0917c06f
Fransjo Leihitu <coreelements@MacBook-Pro-van-Fransjo.local>
2017-07-22 10:01:01 +0200

    CryptoExchangeInterface v0.3 - added comments
    CryptoExchangeInterface v0.2
    - added getCurrencyUrl

--- includes/cryptoexchange.interface.php
+++ includes/cryptoexchange.interface.php
@@ -1,19 +1,30 @@
 <?php
-/*
-*
-* @package    cryptofyer
-* @class CryptoExchangeInterface
-* @author     Fransjo Leihitu
-* @version    0.2
-*
-*/
-interface CryptoExchangeInterface {
+  /*
+  *
+  * @package    cryptofyer
+  * @class CryptoExchangeInterface
+  * @author     Fransjo Leihitu
+  * @version    0.3
+  *
+  */
+  interface CryptoExchangeInterface {
 
-  public function getTicker($args  = null);
-  public function getBalance($args  = null);
-  public function buy($args = null);
-  public function sell($args = null);
-  public function getOrders($args = null) ;
-  public function getCurrencyUrl($args = null);
-}
- ?>
+    // get ticket information
+    public function getTicker($args  = null);
+
+    // get balance
+    public function getBalance($args  = null);
+
+    // place buy order
+    public function buy($args = null);
+
+    // place sell order
+    public function sell($args = null);
+
+    // get open orders
+    public function getOrders($args = null) ;
+
+    // Get the exchange currency detail url
+    public function getCurrencyUrl($args = null);
+  }
+?>
